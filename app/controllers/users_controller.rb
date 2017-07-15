65fe8770c2bf0e2e2aefbee2b78abe0ad4e2bdbc
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2017-07-15 21:45:59 +0530

    Fix name field issue on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def create
     @user = User.find_or_initialize_by(email: user_params[:email])
-    @user.name = user_params[:email]
+    @user.name = user_params[:name]
     @user.auth_token = SecureRandom.hex
     if @user.save!
       render json: @user
