5fe63c160388e732cc5c324b253654c84601f4f3
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2017-07-15 13:28:20 +0530

    Create user model and API to create a user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.find_or_initialize_by(email: user_params[:email])
+    @user.name = user_params[:email]
+    @user.auth_token = SecureRandom.hex
+    if @user.save!
+      render json: @user
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
