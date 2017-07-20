dd27aa97cdf784b48a5f0c6117e185bbe415696c
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2017-07-20 11:32:29 +0530

    Make address details optional in entry
    - This can be nil in some devices

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ApplicationRecord
   belongs_to :user
-  validates_presence_of :uuid, :text, :user_id, :date, :address, :latitude, :longitude
+  validates_presence_of :uuid, :text, :user_id, :date
   validates_uniqueness_of :uuid
 end
--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -17,9 +17,6 @@ RSpec.describe Entry, type: :model do
   it { is_expected.to validate_presence_of :uuid }
   it { is_expected.to validate_presence_of :user_id }
   it { is_expected.to validate_presence_of :date }
-  it { is_expected.to validate_presence_of :latitude }
-  it { is_expected.to validate_presence_of :longitude }
-  it { is_expected.to validate_presence_of :address }
 
   it { is_expected.to validate_uniqueness_of :uuid }
 end
