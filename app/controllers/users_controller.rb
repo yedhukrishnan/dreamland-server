de6a0f37ced78e8176ebbd57b2166e2cb1c7b53c
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2017-07-25 22:45:12 +0530

    Use Google ID token to verify user
    - User is no longer verified using email and name. The ID token
      generated by android will be verified by Google public keys in the
      server.
    - And that makes the app secure :lock:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,29 @@
 class UsersController < ApplicationController
   def create
-    @user = User.find_or_initialize_by(email: user_params[:email])
-    @user.name = user_params[:name]
-    if @user.save!
-      render json: @user
+    payload = verify_id_token(user_params[:id_token])
+    if payload.nil?
+      rendor json: { error: 'User verification failed' }, status: :unprocessable_entity
+    else
+      @user = User.find_or_initialize_by(email: payload['email'])
+      @user.name = payload['name']
+      if @user.save!
+        render json: @user
+      end
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:id_token)
+  end
+
+  def verify_id_token token
+    audience = JWT.decode(token, nil, false).first['aud']
+    if(audience == ENV['ANDROID_CLIENT_ID'])
+      validator = GoogleIDToken::Validator.new(expiry: 1800)
+      return validator.check(token, audience)
+    end
+    false
   end
 end
